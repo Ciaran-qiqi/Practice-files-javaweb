31312a0950fe94f0b249d5fc0dec8b1d131c5009
Ciaran77 <1946501065@qq.com>
2024-12-21 10:19:54 +0800

    部门员工表增删改查初始环境 getmapper

--- src/main/java/com/itheima/controller/DeptController.java
+++ src/main/java/com/itheima/controller/DeptController.java
@@ -1,10 +1,22 @@
 package com.itheima.controller;
 
+import com.itheima.pojo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 部门管理Controller
  */
+@Slf4j
 @RestController
 public class DeptController {
+
+
+    @GetMapping("/depts")
+    public Result list(){
+        log.info("查询全部部门数据");
+        return Result.success();
+    }
+
 }

f76466eee10ec5254a31b52b6b720dff794f25be
Ciaran77 <1946501065@qq.com>
2024-12-21 10:31:52 +0800

    部门员工表增删改查初始环境 查询部门

--- src/main/java/com/itheima/controller/DeptController.java
+++ src/main/java/com/itheima/controller/DeptController.java
@@ -1,9 +1,14 @@
 package com.itheima.controller;
 
+import com.itheima.pojo.Dept;
 import com.itheima.pojo.Result;
+import com.itheima.service.DeptService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 部门管理Controller
@@ -12,11 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DeptController {
 
+    @Autowired
+    private DeptService deptService;
+
 
     @GetMapping("/depts")
     public Result list(){
         log.info("查询全部部门数据");
-        return Result.success();
+
+        //调用业务层查询全部部门数据
+        List<Dept> deptList = deptService.list();
+
+        return Result.success(deptList);
     }
 
 }
--- src/main/java/com/itheima/mapper/DeptMapper.java
+++ src/main/java/com/itheima/mapper/DeptMapper.java
@@ -1,10 +1,20 @@
 package com.itheima.mapper;
 
+import com.itheima.pojo.Dept;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * 部门管理
  */
 @Mapper
 public interface DeptMapper {
+    /*
+     * 查询所有部门
+     * @Return
+     */
+    @Select("select * from dept")
+    List<Dept> list();
 }

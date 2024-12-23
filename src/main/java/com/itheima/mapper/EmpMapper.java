5ea4e0ca5521f696714b5fcbaee7a325ed03d804
Ciaran-qiqi <ciaran.aicheng@gmail.com>
2024-12-23 14:34:21 +0800

    员工表-动态条件查询-分页查询

--- src/main/java/com/itheima/mapper/EmpMapper.java
+++ src/main/java/com/itheima/mapper/EmpMapper.java
@@ -1,10 +1,17 @@
 package com.itheima.mapper;
 
+import com.itheima.pojo.Emp;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  * 员工管理
  */
 @Mapper
 public interface EmpMapper {
+    //获取当前页的结果列表(用xml动态配置sql)
+    public List<Emp> list(String name, Short gender, LocalDate begin, LocalDate end);
 }
--- src/main/java/com/itheima/service/EmpService.java
+++ src/main/java/com/itheima/service/EmpService.java
@@ -1,7 +1,22 @@
 package com.itheima.service;
 
+import com.itheima.pojo.PageBean;
+
+import java.time.LocalDate;
+
 /**
  * 员工管理
  */
 public interface EmpService {
+    /**
+     * 条件分页查询
+     * @param page     页码
+     * @param pageSize 每页展示记录数
+     * @param name     姓名
+     * @param gender   性别
+     * @param begin   开始时间
+     * @param end     结束时间
+     * @return
+     */
+    PageBean page(Integer page, Integer pageSize, String name, Short gender, LocalDate begin, LocalDate end);
 }
